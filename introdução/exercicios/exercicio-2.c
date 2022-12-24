b078b145183a52efe4942797cda9dc0c3753c17f
gonzagadavid <david.blesses@hotmail.com>
2022-12-24 18:03:53 -0300

    feat: add introduction exercise 2

---
+++ introdução/exercicios/exercicio-2.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+// 2 - faça um programa que leia um número real e o imprima
+
+
+int main () {
+  float num;
+
+  printf("digite um número real:\n");
+  scanf("%f", &num);
+
+  printf("o número real digitado foi %f\n", num);
+
+  return 0;
+}
