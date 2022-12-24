51d3338b09a2b7878f64faf93d9b3e1c514a39aa
gonzagadavid <david.blesses@hotmail.com>
2022-12-24 17:14:07 -0300

    feat: add introduction exercise 1

---
+++ introdução/exercicios/exercicio-1.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+// 1 - faça um programa que leia um número inteiro e o imprima
+
+
+int main () {
+  int num;
+
+  printf("digite um número inteiro:\n");
+  scanf("%d", &num);
+
+  printf("o número inteiro digitado foi %d\n", num);
+
+  return 0;
+}
